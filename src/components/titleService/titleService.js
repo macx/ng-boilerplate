48052820df18c7ebad1e2c6432c38a7b48ae0896
Pascal Precht <pascal.precht@googlemail.com>
2013-06-17 11:10:00 +0200

    feat(ngdocs): add ngdocs task
    - adds grunt-ngdocs with github endpoint as dev dependency
    - adds doc folder with sections for api, tutorial and guide
    - adds additional html and css for generated ngdocs site
    - update src titleService with propery comments to show how docs can be generated
    
    Closes #13

--- src/components/titleService/titleService.js
+++ src/components/titleService/titleService.js
@@ -2,16 +2,82 @@
 
 angular.module('titleService', [])
 
+/**
+ * @ngdoc object
+ * @name titleService.titleService
+ *
+ * @description
+ * A simple title service to set the title of an HTML document at runtime.
+ */
 .factory('titleService', function($document) {
   var suffix, title;
 
   var titleService = {
+    /**
+     * @ngdoc function
+     * @name titleService.titleService#setSuffix
+     * @methodOf titleService.titleService
+     *
+     * @description
+     * Use `setSuffix()` to set a title suffix which get appended on the html document
+     * title.
+     *
+     * @param {string} s Suffix as string
+     *
+     * @example
+     * <pre>
+     *  angular.module('myApp', ['titleService']).run(function (titleService) {
+     *    titleService.setSuffix(' | ngBoilerplate');
+     *  });
+     * </pre>
+     */
     setSuffix: function setSuffix(s) {
       suffix = s;
     },
+    /**
+     * @ngdoc function
+     * @name titleService.titleService#getSuffix
+     * @methodOf titleService.titleService
+     *
+     * @description
+     * Returns configured suffix which has been set with  `setSuffix()`.
+     *
+     * @returns {string}  Configured suffix.
+     *
+     * @example
+     * <pre>
+     *  angular.module('myApp', ['titleService']).run(function (titleService) {
+     *    // set suffix
+     *    titleService.setSuffix(' | ngBoilerplate');
+     *
+     *    // get suffix
+     *    titleService.getSuffix();
+     *  });
+     * </pre>
+     */
     getSuffix: function getSuffix() {
       return suffix;
     },
+
+    /**
+     * @ngdoc function
+     * @name titleService.titleService#setTitle
+     * @methodOf titleService.titleService
+     *
+     * @description
+     * Let you set a title for a specific view on a specific controller.
+     *
+     * @param {string} t Title prefix.
+     *
+     * @example
+     * <pre>
+     *  angular.module('myApp', ['titleService']);
+     *
+     *  angular.module('Ctrl', function (titleService) {
+     *    titleService.setTitle('home');
+     *  });
+     * </pre>
+     */
     setTitle: function setTitle(t) {
       if (angular.isDefined(suffix)) {
         title = t + suffix;
@@ -21,6 +87,29 @@ angular.module('titleService', [])
 
       $document.prop('title', title);
     },
+
+    /**
+     * @ngdoc function
+     * @name titleService.titleService#getTitle
+     * @methodOf titleService.titleService
+     *
+     * @description
+     * Returns currently configured HTML document title.
+     *
+     * @returns {string} Configured HTML document title.
+     *
+     * @example
+     * <pre>
+     *  angular.module('myApp', ['titleService']);
+     *
+     *  angular.module('Ctrl', function (titleService) {
+     *    titleService.setTitle('home');
+     *
+     *    // get title
+     *    titleService.getTitle();
+     *  });
+     * </pre>
+     */
     getTitle: function getTitle() {
       return $document.prop('title');
     }
