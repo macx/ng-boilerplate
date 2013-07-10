2cd168b9b7711a4f034ed838201db09402c93dd9
Johannes Troeger <johannes.troeger@gmail.com>
2013-07-10 08:50:27 +0200

    fix(gruntfile): connect server has to be available from outside

--- Gruntfile.js
+++ Gruntfile.js
@@ -393,7 +393,7 @@ module.exports = function (grunt) {
         options: {
           port: 9000,
           // change this to '0.0.0.0' to access the server from outside
-          hostname: 'localhost',
+          hostname: '*',
           middleware: function (connect) {
             return [lrSnippet, mountFolder(connect, 'build')];
           }
