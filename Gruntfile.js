edd90bd27b8f32ce48361da63dd4f339b4a1f214
Johannes Troeger <johannes.troeger@gmail.com>
2013-07-22 14:56:01 +0200

    fix(grunt): updates to correct bower.json path

--- Gruntfile.js
+++ Gruntfile.js
@@ -79,7 +79,7 @@ module.exports = function (grunt) {
         commitMessage: 'chore(release): v%VERSION%',
         commitFiles: [
           'package.json',
-          'client/bower.json'
+          'bower.json'
         ],
         createTag: false,
         tagName: 'v%VERSION%',
